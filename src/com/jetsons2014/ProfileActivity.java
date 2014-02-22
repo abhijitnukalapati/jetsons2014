49e81cfbac3432fa7a4bb3efc164dc4ec04794ab
BradMoeller <brad@brandingbrand.com>
2014-02-22 13:38:04 -0500

    initial app shell added

---
+++ src/com/jetsons2014/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.jetsons2014;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProfileActivity extends BaseActivity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
